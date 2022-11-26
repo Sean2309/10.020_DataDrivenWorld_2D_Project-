e98d108ecf22b80a6c479cae1690de6a95a20d7b
Sean Phay <seanphay@gmail.com>
2022-11-26 12:25:16 +0800

    Cleaned Repo

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 2D_DDW_LinReg
+
+## Overview of Project
+1. Code -> Consists of scripts that perform 4 main functions:
+- Exploratory Data Analysis (EDA)
+- Modelling (Linear Regression Model)
+- Processing (Data)
+- Results
+2. Datasets
+3. Webpage (Flask Template)
--- webpage/app/__init__.py
+++ webpage/app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from config import Config
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap5
 from app.middleware import PrefixMiddleware
 
 application = Flask(__name__)
 application.config.from_object(Config)
-bootstrap = Bootstrap(application)
+bootstrap = Bootstrap5(application)
 
 # set voc=False if you run on local computer
 application.wsgi_app = PrefixMiddleware(application.wsgi_app, voc=False)
